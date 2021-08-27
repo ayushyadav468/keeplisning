945bfca1da91993995a026591e5a1c3a9156ae6c
Ayush Yadav <ayushyadav468@gmail.com>
2021-08-27 18:02:34 +0530

    added styled-component and custom fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { GlobalStyle } from '../styles/GlobalStyles.style';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+	return (
+		<>
+			<GlobalStyle />
+			<Component {...pageProps} />
+		</>
+	);
 }
-export default MyApp
